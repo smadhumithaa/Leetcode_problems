3bff2705ea73c09992fce62b2afa0012734ee704
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-10-11 12:50:02 +0530

    Create Longest Uncommon Subsequence I.java

---
+++ Contest_1st_priblems/Longest Uncommon Subsequence I.java
@@ -0,0 +1,9 @@
+Weekly contest 26
+Longest Uncommon Subsequence I
+public class Solution {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b))
+            return -1;
+        return Math.max(a.length(), b.length());
+    }
+}
