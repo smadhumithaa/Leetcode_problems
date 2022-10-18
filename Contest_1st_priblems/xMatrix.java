0bc7b06a7346139e3fffba75ae2551b9faff1d00
smadhumithaa <112147052+smadhumithaa@users.noreply.github.com>
2022-10-18 10:20:52 +0530

    Create xMatrix.java

---
+++ Contest_1st_priblems/xMatrix.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean checkXMatrix(int[][] grid) {
+        int n=grid.length;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                if(i==j || j+i+1==n){
+                    if(grid[i][j]==0)return false;
+                }
+                else if(grid[i][j]>0) return false;
+            }
+        }
+        return true;
+    }
+}
